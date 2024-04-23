b68e8e24fd05114e677e87b650f9b187212f1112
ferpa <feririarte.1996@gmail.com>
2024-04-22 23:34:15 -0300

    add app theme

--- src/context/AppProvider.tsx
+++ src/context/AppProvider.tsx
@@ -1,11 +1,12 @@
 "use client";
+import { appTheme } from "@/config/theme";
 import { NostrProvider } from "@lawallet/react";
 import { NextProvider } from "@lawallet/ui/next";
 import React, { Suspense } from "react";
 
 const AppProvider = ({ children }: { children: React.ReactNode }) => {
   return (
-    <NextProvider>
+    <NextProvider theme={appTheme}>
       <NostrProvider>
         <Suspense>{children}</Suspense>
       </NostrProvider>
