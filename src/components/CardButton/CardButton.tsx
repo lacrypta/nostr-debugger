398893a72038a96c5f1836c5dca7ffae18322302
ferpa <feririarte.1996@gmail.com>
2024-04-24 11:59:00 -0300

    fix cursor pointer

--- src/components/CardButton/CardButton.tsx
+++ src/components/CardButton/CardButton.tsx
@@ -1,6 +1,4 @@
-import { appTheme } from "@/config/theme";
-import { ButtonSetting, CaretRightIcon, Flex, Icon, Text } from "@lawallet/ui";
-import React from "react";
+import { Flex, LinkSetting, Text } from "@lawallet/ui";
 
 const CardButton = ({
   text,
@@ -10,15 +8,11 @@ const CardButton = ({
   onClick: () => void;
 }) => {
   return (
-    <ButtonSetting onClick={onClick}>
+    <LinkSetting onClick={onClick}>
       <Flex flex={1} align="start" justify="start">
         <Text isBold={true}>{text}</Text>
       </Flex>
-
-      <Icon size="small" color={appTheme.colors.gray40}>
-        <CaretRightIcon />
-      </Icon>
-    </ButtonSetting>
+    </LinkSetting>
   );
 };
 
